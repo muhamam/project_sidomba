0dc6e3bd1ff667fa7a5a37df1c92836377771c43
IndraRusyana <indrarusyana@gmail.com>
2022-08-14 07:51:04 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes 
+| Web Routes test
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
