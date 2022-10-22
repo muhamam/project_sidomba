c7881a69fdbb0e06b2d1fefce1469ed4163e7706
muhammad anas aiman <muhammadanasaiman0@gmail.com>
2022-10-22 21:39:16 +0700

    menambahkan layour sidebar pada halaman user

--- resources/views/user/tulis_ulasan_pembeli.blade.php
+++ resources/views/user/tulis_ulasan_pembeli.blade.php
@@ -3,62 +3,7 @@
 @section('content')
 
 <div class="row">
-    <div class="col " style="margin-left: 30px; margin-top:180px; width: 300px;">
-        <div class=" bg-light ">
-            <center>
-                <table>
-                    <tr>
-                        <td> <span><img src="../img/about-us.jpg" alt="" style="width: 85px; height:85px; "
-                                    class="  ml-3 rounded-circle">
-                            </span></td>
-                        <td>
-                            <a href="#" class="  " style="color:#000000 ;">Muhammad
-                                anas
-                            </a>
-                            <a href="#" style="font-size:9px;color:#000000 ;">muhammadanasaiman0@gmail.com</a><br>
-                            <ul class="d-flex" style="list-style-type: disc; color:red; font-size:12px;">
-                                <li>
-                                    <span style="color: black;">Peternakan</span>
-                                </li>
-                                <li class="ml-4">
-                                    <span style="color: black;">Investasi</span>
-                                </li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                    </tr>
-                </table>
-                <div class="garis" style="width: 95%; height: 2px; background-color: black; margin-left: 5px; margin-right:
-                        5px;">
-                    <p></p>
-                </div>
-                <div class="dalam ">
-                    <span class="iconify" data-icon="ic:baseline-account-box" data-width="25" data-height="25"
-                        style="margin-right: 130px; margin-bottom: -60px;"></span>
-                    <a href="dashboard_investor.html" class="dropdown-item ">Akun</a>
-                    <span class="iconify" data-icon="bi:chat-text" data-width="25" data-height="25"
-                        style="margin-right: 130px; margin-bottom: -60px;"></span>
-                    <a href="pasar.html" class="dropdown-item ">Chat</a>
-                    <span class="iconify" data-icon="carbon:star-review" data-width="25" data-height="25"
-                        style="margin-right: 130px; margin-bottom: -60px;"></span>
-                    <a href="investasi_saya_semua.html" class="dropdown-item">Ulasan</a>
-                    <span class="iconify" data-icon="carbon:warning" data-width="25" data-height="25"
-                        style="margin-right: 130px; margin-bottom: -60px;"></span>
-                    <a href="investasi_riwayat.html" class="dropdown-item">Komplain</a>
-                    <span class="iconify" data-icon="bi:list-stars" data-width="25" data-height="25"
-                        style="margin-right: 130px; margin-bottom: -60px;"></span>
-                    <a href="#" class="dropdown-item ml-3">Wishlist</a>
-                    <span class="iconify" data-icon="carbon:money" data-width="25" data-height="25"
-                        style="margin-right: 130px; margin-bottom: -60px;"></span>
-                    <a href="#" class="dropdown-item ml-3 ">Transaksi</a>
-                </div>
-
-            </center>
-        </div>
-
-    </div>
+    @include('layouts.sidebarUser')
     <section class="ftco-section col " style="margin-top: 95px;">
         <div class="tengah d-flex d-flex justify-content-center " style="margin-bottom: 20px; margin-top:-50px;">
 
