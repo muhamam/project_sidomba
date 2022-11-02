98f36476fd844f3218e41993d617497e039f1040
muhammad anas aiman <muhammadanasaiman0@gmail.com>
2022-11-02 12:56:03 +0700

    merapihkan aktivasi investor

--- resources/views/user/aktivasi_investor.blade.php
+++ resources/views/user/aktivasi_investor.blade.php
@@ -2,26 +2,18 @@
 
 @section('content')
 
-<div class="container mt-4" style="background-color: white;">
+<div class="form-pendaftaran rounded pb-5" style="background-color: #E5E5E5;">
 
-    <div class="d-flex d-flex justify-content-center my-5">
-        <img src="../img/domba.png" alt="" style="width: 77px; height: 77px;">
-
-        <div class="garis"
-            style="width: 4px; height: 85px; background-color: black; margin-left: 5px; margin-right: 5px;">
-            <p></p>
-        </div>
-
-
-        <h3 class="" style="margin-top: 10px; margin-left: 10px; height: 10px;">CARIBI</h3>
-        <h4 class="" style="margin-top: 40px; margin-left: -120px;">Investasi</h4>
+    <div class="head-peternak d-flex d-flex justify-content-center my-5 pt-5">
+        <img src="../../img/domba.png" alt="" style="width: 97px; height: 97px;">
+        <h3 class="text-dark">CARIBI</h3>
+        <h4 class="">Investasi</h4>
     </div>
-
     <!-- Banner Starts Here -->
     <div class="col-md-10 rounded mx-auto p-5"
         style="background-image: url({{asset('img/banner-bg.jpg')}});background-size:cover; background-repeat:no-repeat;">
         <div class="text-white text-center" style="background:rgba(0,0,0, 0.7);">
-            <h2>SELANGKAH LAGI MENJADI SEORANG INVESTOR DOMBA HANDAL!</h2>
+            <h2 class="text-white">SELANGKAH LAGI MENJADI SEORANG INVESTOR DOMBA HANDAL!</h2>
             <p style="color: white;">Dengan SI-Domba, kami membuka peluang investasi dalam bentuk ternak domba. Hanya
                 dengan
                 mengisi beberapa form dibawah ini, akun investor Anda bisa langsung diverifikasi, dan bisa diaktifkan
@@ -30,45 +22,43 @@
         </div>
     </div>
     <!-- Banner Ends Here -->
-    <center>
-        <div class="col-10">
-            <center>
-                <h2>Aktivasi Akun Investor</h2>
-            </center>
-            <div class="form-group col-8">
-                <label for="formGroupExampleInput">
-                    <h6>NIK (Nomor Induk Kependudukan)</h6>
-                </label>
-                <input type="text" class="form-control" id="formGroupExampleInput"
-                    placeholder="NIK sesuai yang tertera pada KTP Anda">
-            </div>
-            <form>
-                <div class="form-group col-8">
-                    <label for="exampleFormControlFile1">Upload Foto KTP</label>
-                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                </div>
-            </form>
-            <br>
-            <form>
-                <div class="form-group col-8">
-                    <label for="exampleFormControlFile1">
-                        <h4> Upload Foto Anda memegang KTP</h4>
-                    </label>
-                    <h5>Pastikan Wajah dan KTP Anda Terlihat dengan Jelas</h5>
-                    <br>
-                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                    <br>
-                    <p>Contoh foto yang benar :</p>
-                    <img src="../img/ktp2.jpeg" alt="" width="220px" height="150px">
-
-                    <center>
-                        <div class="dotted mt-5 col-md-10"></div>
-                    </center>
-
-                </div>
-            </form>
+    <div class="form-inti-peternakan col-10 mt-5 rounded mb-5">
+        <center>
+            <h2>Aktivasi Akun Investor</h2>
+        </center>
+        <div class="form-group col-11">
+            <label for="formGroupExampleInput">
+                <h5 class="text-dark ">NIK (Nomor Induk Kependudukan)</h5>
+            </label>
+            <input type="text" class="form-control" id="formGroupExampleInput"
+                placeholder="NIK sesuai yang tertera pada KTP Anda">
         </div>
-    </center>
+        <form>
+            <div class="form-group col-11">
+                <label for="exampleFormControlFile1">
+                    <h5 class="text-dark ">Upload Foto KTP</h5>
+                </label>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+        </form>
+        <br>
+        <form>
+            <div class="form-group col-11">
+                <label for="exampleFormControlFile1">
+                    <h4> Upload Foto Anda memegang KTP</h4>
+                    <h5 class="text-dark ">Pastikan Wajah dan KTP Anda Terlihat dengan Jelas</h5>
+                </label>
+                <br>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                <br>
+                <p>Contoh foto yang benar :</p>
+                <center>
+                    <img src="../../img/ktp2.jpeg" alt="" width="220px" height="250px">
+                </center>
+
+            </div>
+        </form>
+    </div>
 </div>
 
 
